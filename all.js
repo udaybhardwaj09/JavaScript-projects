04aae8d460d0b74f00fa12a02284462c8d4a579f
udaybhardwaj09 <udaybhardwaj.2006@gmail.com>
2026-06-30 10:53:48 +0530

    Update all.js

--- all.js
+++ all.js
@@ -532,8 +532,48 @@ function formatDates(element){
 
 // SWAP 2 ELEMENTS IN AN ARRAY
 
-const colors = ['red', 'green', 'blue', 'black', 'white'];
+// const colors = ['red', 'green', 'blue', 'black', 'white'];
+// [colors[0], colors[4]] = [colors[4], colors[0]]
+// console.log(colors);
 
-[colors[0], colors[4]] = [colors[4], colors[0]]
 
-console.log(colors);
+/********************/
+//Nested Objects
+
+class Person{
+
+    constructor(name, age, ...address){
+        this.name = name;
+        this.age = age;
+        this.address = new Address(...address);
+    }
+}
+
+class Address{
+
+    constructor(street, city, country){
+        this.street = street;
+        this.city = city;
+        this.country = country;
+    }
+}
+
+const person1 = new Person("Spongebob", 30, "124 Conch St.", 
+                                    "Bikini Bottom", 
+                                    "Int. Waters");
+
+const person2 = new Person("Patrick", 37, "128 Conch St.", 
+                                    "Bikini Bottom", 
+                                    "Int. Waters");
+
+const person3 = new Person("Squidward", 45, "126 Conch St.", 
+                                    "Bikini Bottom", 
+                                    "Int. Waters");
+
+
+console.log(person1.name);
+console.log(person1.age); 
+console.log(person1.address);                                   
+console.log(person1.address.street);
+console.log(person1.address.city);
+console.log(person1.address.country);
