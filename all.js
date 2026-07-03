6702f1e8a2986a8ca5e0148ae185c879c1eb8ef4
udaybhardwaj09 <udaybhardwaj.2006@gmail.com>
2026-07-04 01:21:59 +0530

    synchronous and asynchronous

--- all.js
+++ all.js
@@ -641,3 +641,21 @@ function formatDates(element){
 
 /**********************************/
 
+// Synchronous & Asynchronous
+
+// function func1(callback){
+//     setTimeout(() => {
+//         console.log("Task 1");
+//         callback();
+//     },3000);
+    
+// }
+
+// function func2(){
+//     console.log("Task 2");
+//     console.log("Task 3");
+//     console.log("Task 4");
+// }
+
+// func1(func2);
+
