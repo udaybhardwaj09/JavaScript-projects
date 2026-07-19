e020a78ad6455d2d159075f6c70b51e17282c535
udaybhardwaj09 <udaybhardwaj.2006@gmail.com>
2026-07-19 21:48:08 +0530

    javascript of Image Slider

---
+++ image-slider/script.js
@@ -0,0 +1,38 @@
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
+
+function showSlide(index){
+    if(index >= slides.length){
+        index = 0;
+    }
+    else if(index < 0){
+        index = slides.length - 1;
+    }
+
+    slideIndex = index;
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide(){
+    slideIndex++;
+    showSlide(slideIndex);
+}
