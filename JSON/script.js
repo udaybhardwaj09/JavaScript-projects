1be2755c3e5904be364f9f0e5d7ea5f026cc2ee7
udaybhardwaj09 <udaybhardwaj.2006@gmail.com>
2026-07-21 12:29:10 +0530

    JSON with fetch

---
+++ JSON/script.js
@@ -0,0 +1,55 @@
+// ---------- JSON.stringify() ----------
+
+const names = ["Spongebob", "Patrick", "Squidward", "Sandy"];
+const person = {
+    "name": "Spongebob",
+    "age": 30,
+    "isEmployed": true,
+    "hobbies": ["Jellyfishing", "Karate", "Cooking"]
+};
+const people = [{
+    "name": "Spongebob",
+    "age": 30,
+    "isEmployed": true
+},
+{
+    "name": "Patrick",
+    "age": 34,
+    "isEmployed": false
+},
+{
+    "name": "Squidward",
+    "age": 50,
+    "isEmployed": true
+},
+{
+    "name": "Sandy",
+    "age": 27,
+    "isEmployed": false
+}];
+
+// const jsonString = JSON.stringify(people);
+
+// console.log(jsonString);
+
+
+// ---------- JSON.parse() ----------
+
+const jsonNames = `["Spongebob", "Patrick", "Squidward", "Sandy"]`;
+const jsonPerson = `{"name": "Spongebob", "age": 30, "isEmployed": true, "hobbies": ["Jellyfishing", "Karate", "Cooking"]}`;
+const jsonPeople = `[{"name": "Spongebob","age": 30,"isEmployed": true},
+                     {"name": "Patrick","age": 34,"isEmployed": false},
+                     {"name": "Squidward","age": 50,"isEmployed": true},
+                     {"name": "Sandy","age": 27,"isEmployed": false}]`;
+
+const parsedData = JSON.parse(jsonPeople);
+
+console.log(parsedData);
+
+
+// ---------- fetch() ----------
+
+fetch("people.json")
+    .then(response => response.json())
+    .then(values => values.forEach(value => console.log(value)))
+    .catch(error => console.error(error));
