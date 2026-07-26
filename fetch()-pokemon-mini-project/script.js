2d895d6388c8065a2ae65498b40816afc6ee5a86
udaybhardwaj09 <udaybhardwaj.2006@gmail.com>
2026-07-26 16:43:43 +0530

    Pokemon API fetch

--- fetch()-pokemon-mini-project/script.js
+++ fetch()-pokemon-mini-project/script.js
@@ -1,11 +1,36 @@
 // fetch()
 
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-    .then(response => {
+// fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+//     .then(response => {
+//         if(!response.ok){
+//             throw new Error("Could not fetch resource");
+//         }
+//         return response.json();
+//     })
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
+
+
+// fetchData();
+async function fetchData() {
+    try{
+
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
         if(!response.ok){
             throw new Error("Could not fetch resource");
         }
-        return response.json();
-    })
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+
+        const data = await response.json(); //.json converts the string from server to js obj
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    }
+    catch(error){
+        console.error(error);
+    }
+}
